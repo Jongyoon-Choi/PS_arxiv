efb947c624ab77fe35bd42ca6fe9bdce1b7a0ab4
최종윤 <129063106+Jongyoon-Choi@users.noreply.github.com>
2026-03-01 18:04:21 +0900

    [Silver II] Title: 좌표 압축, Time: 3188 ms, Memory: 377992 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Arrays;
+
+class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N= sc.nextInt();
+		int[] origin = new int [N];
+		int[] sorted = new int [N];
+		HashMap<Integer, Integer> rankingMap = new HashMap<Integer, Integer>();
+		for (int i=0;i<N;i++) {
+			sorted[i]=origin[i]=sc.nextInt();
+		}
+		Arrays.sort(sorted);
+		int rank=0;
+		for (int j:sorted) {
+			if(!rankingMap.containsKey(j)) {
+				rankingMap.put(j, rank);
+				rank++;		// map에 넣고나면 다음 순위를 가리킬 수 있도록 1을 더해준다.
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int key : origin) {
+			int ranking = rankingMap.get(key);	// 원본 배열 원소(key)에 대한 value(순위)를 갖고온다.
+			sb.append(ranking).append(' ');
+		}
+		System.out.println(sb);
+	}
+}
